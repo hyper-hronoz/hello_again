4efb5856c667a30dfb7a52dc5624089123b712b5
Your Name <you@example.com>
2022-04-14 09:29:37 +0300

    image addition

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route("/index/<title>", methods=["GET"])
 def hello(title):
-    return render_template("index.html", title=title)
+    image_source = "/static/1.jpg"
+
+    if ("инженер" in title or "строитель" in title):
+        image_source = "/static/2.png"
+
+    return render_template("index.html", title=title, image_source=image_source)
