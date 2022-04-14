cbbc47ee79496c1ded342765c0b779fabb8c48cb
Your Name <you@example.com>
2022-04-14 12:39:43 +0300

    rainbow

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, escape, request, render_template
+from email.policy import default
+from multiprocessing import context
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/list_prof", methods=["GET"], defaults={"list_type": ""})
-@app.route("/list_prof/<list_type>", methods=["GET"])
-def hello(list_type):
+planet_info = {
+    "venus": ["Не живая", "Горячо", "Кислотные дожди", "Godness of decay"],
+    "nothing": ["Enter planet name"]
+}
+
+@app.route("/choice", defaults={"planet" : "nothing"})
+@app.route("/choice/", defaults={"planet" : "nothing"})
+@app.route("/choice/<planet>")
+def choice(planet):
     context = {}
+    if planet in planet_info:
+        context["facts"] = planet_info[planet]
 
-    context["error"] = ""
-
-    if not list_type:
-        context["error"] = "Да я тебя на марс отправлю! Вписал параметр быстро!"
-    else:
-        context["professions"] = ["инженер-исследователь", "пилот", "строитель", "экзобиолог", "не могли сразу готовый массив дать?"]
-
-    context["list_type"] = list_type
-
+    context["planet_name"] = planet
     return render_template("index.html", context=context)
